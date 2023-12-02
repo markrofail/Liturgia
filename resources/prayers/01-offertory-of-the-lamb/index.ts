a0b71932a0f6195690b6f9b78e3757b9d00cd917
Mark Rofail <markm.rofail@gmail.com>
2023-12-02 20:14:58 +0000

    Update liturgy text

--- resources/prayers/01-offertory-of-the-lamb/index.ts
+++ resources/prayers/01-offertory-of-the-lamb/index.ts
@@ -3,7 +3,10 @@ export default [
     require("./01-introduction-to-the-creed.json"),
     require("./02-the-orthodox-creed.json"),
     require("./03-procession-of-the-lamb.json"),
-    require("./04-khen-efran.json"),
-    require("./05-thanksgiving-prayer.json"),
-    require("./06-absolution-of-the-servants.json"),
+    require("./04-lord-have-mercy.json"),
+    require("./05-holy-holy-holy.json"),
+    require("./06-alleluia-fai-be-pi.json"),
+    require("./07-khen-efran.json"),
+    require("./08-thanksgiving-prayer.json"),
+    require("./09-absolution-of-the-servants.json"),
 ];
--- resources/prayers/02-liturgy-of-the-word/index.ts
+++ resources/prayers/02-liturgy-of-the-word/index.ts
@@ -6,6 +6,7 @@ export default [
     require("./05-praxis-response.json"),
     require("./06-praxis.json"),
     require("./07-synaxarion.json"),
-    require("./08-litany-of-the-gospel.json"),
-    require("./09-psalm-and-gospel.json"),
+    require("./08-the-trisagion.json"),
+    require("./09-litany-of-the-gospel.json"),
+    require("./10-psalm-and-gospel.json"),
 ];
