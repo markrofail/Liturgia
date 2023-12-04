e67e855c51f89026fdeac5155b9c5b5d554f5595
Mark Rofail <markm.rofail@gmail.com>
2023-12-04 10:06:39 +0000

    Move all prayer text to assets

--- resources/prayers/04-distribution/index.ts
+++
@@ -1,6 +0,0 @@
-export default [
-    require("./01-introduction.json"),
-    require("./02-psalm-150.json"),
-    require("./03-our-father-who-art-in-heaven.json"),
-    require("./04-our-savior-called-upon-us.json"),
-]
