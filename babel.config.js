da3f41abe9ce90c460649be851d6d52738dd6ffb
Mark Rofail <markm.rofail@gmail.com>
2023-12-17 21:04:19 +0000

    Update alias paths

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,23 @@
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ["babel-preset-expo"],
-    plugins: ["react-native-reanimated/plugin"],
-    env: {
-      production: {
-        plugins: ["react-native-paper/babel"],
-      },
-    },
-  };
+module.exports = function (api) {
+    api.cache(true);
+    return {
+        presets: ["babel-preset-expo"],
+        plugins: [
+            "react-native-reanimated/plugin",
+            [
+                "module-resolver",
+                {
+                    alias: {
+                        "@assets": "./assets",
+                    },
+                    extensions: [".js", ".jsx", ".ts", ".tsx"],
+                },
+            ],
+        ],
+        env: {
+            production: {
+                plugins: ["react-native-paper/babel"],
+            },
+        },
+    };
 };
